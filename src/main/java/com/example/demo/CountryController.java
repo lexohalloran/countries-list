8178b5117f8e2fd0e7e4423fdb833108f3625d2f
Lex O'Halloran <ohalloran.lex@gmail.com>
2020-10-25 23:06:47 -0400

    second commit

--- src/main/java/com/example/demo/CountryController.java
+++ src/main/java/com/example/demo/CountryController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class CountryController {
 
